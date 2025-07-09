202787e054075d7d977c33c5f95c79e04de68796
akp05 <akhilpradhan547@gmail.com>
2025-07-09 19:29:18 +0530

    Code

---
+++ src/main/java/io/swagger/api/factories/StoreApiServiceFactory.java
@@ -0,0 +1,13 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.StoreApiService;
+import io.swagger.api.impl.StoreApiServiceImpl;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2025-07-09T13:17:56.007+05:30")
+public class StoreApiServiceFactory {
+    private final static StoreApiService service = new StoreApiServiceImpl();
+
+    public static StoreApiService getStoreApi() {
+        return service;
+    }
+}
